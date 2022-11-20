4b2c80b98c74ada71907a5fcd755fcf1602e4d11
CodeZhang <58677740+mike-just@users.noreply.github.com>
2022-11-21 00:50:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,19 +21,19 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '龙哲',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oS3E26Bsj2lbGLmPuj7tmqW4gPks',
+      id: 'oS3E26JqlWjkvVuquawvNNOMjpDc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yyQySw3Fsx5nlu7okxdCRyYZoO9dSKk2D2g07vTYIBM',
+      useTemplateId: 'bN1q1aP4ICenx7IR4jQUkxKoOZOWjHyNefceqD1rMoo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-01',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2022', date: '11-01',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '我的', year: '2022', date: '09-28',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
